c186a9e23e01340425ccbcf651a7b074741ca177
Onyeriri Victor <viktorchinonso@gmail.com>
2023-03-18 14:13:56 +0000

    Added animals to the list of animals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import AnimalShow from "./AnimalShow";
+import { useState } from "react";
+
+const getRandomAnimal = () => {
+  const animals = ["Cow", "Zebra", "Horse", "Lion", "Sheep", "Panda's"];
+  let randomNumber = Math.floor(Math.random() * animals.length);
+  const selectedAnimal = animals[randomNumber];
+
+  return selectedAnimal;
+};
 
 function App() {
+  const [animals, setAnimal] = useState([]);
+
+  const handleClick = () => {
+    setAnimal([...animals, getRandomAnimal()]);
+  };
+
+  const renderAnimal = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
-    <div className="App">
-      <AnimalShow />
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <h1>{renderAnimal}</h1>
     </div>
   );
 }
