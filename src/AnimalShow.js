c97bdb5bc851d89bc8d211dd0a8c8059a9c18324
Onyeriri Victor <viktorchinonso@gmail.com>
2023-03-18 15:46:14 +0000

    Styling heart image and adding click functionality

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -4,6 +4,8 @@ import cow from "./svg/cow.svg";
 import gator from "./svg/gator.svg";
 import horse from "./svg/horse.svg";
 import dog from "./svg/dog.svg";
+import heart from "./svg/heart.svg";
+import { useState } from "react";
 
 const svgMap = {
   bird,
@@ -15,7 +17,18 @@ const svgMap = {
 };
 
 function AnimalShow({ type }) {
-  return <div>{svgMap[type]}</div>;
+  const [clicks, setClicks] = useState(0);
+
+  function handleClick() {
+    setClicks(clicks + 1);
+  }
+
+  return (
+    <div onClick={handleClick}>
+      <img src={svgMap[type]} alt="animals" />
+      <img src={heart} alt="heart" style={{ width: 10 + 10 * clicks + "px" }} />
+    </div>
+  );
 }
 
 export default AnimalShow;
