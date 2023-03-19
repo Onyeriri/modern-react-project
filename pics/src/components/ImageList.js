97d34abd23459748af8dcfb31fea8ccf4e494418
Onyeriri Victor <viktorchinonso@gmail.com>
2023-03-19 12:12:00 +0000

    Rendered images to the App component

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,5 +1,13 @@
-function ImageList() {
-  return <div>Search bar here!!!</div>;
+import ImageShow from "./ImageShow";
+import "./ImageList.css";
+
+function ImageList({ urls }) {
+  const renderedImages = urls.map((url, index) => {
+    console.log(url.urls.regular);
+    return <ImageShow url={url.urls.regular} key={index} />;
+  });
+
+  return <div className="image-list-container">{renderedImages}</div>;
 }
 
 export default ImageList;
--- pics/src/components/ImageShow.js
+++ pics/src/components/ImageShow.js
@@ -1,5 +1,11 @@
-function ImageShow() {
-  return <div>Search bar here!!!</div>;
+import "./ImageShow.css";
+
+function ImageShow({ url }) {
+  return (
+    <div className="image-container">
+      <img className="image-size" src={url} alt="unsplash images" />
+    </div>
+  );
 }
 
 export default ImageShow;
