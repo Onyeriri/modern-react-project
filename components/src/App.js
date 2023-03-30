1b68982a8646da684e3cd980a0ee74cc9aa3d90a
Onyeriri Victor <viktorchinonso@gmail.com>
2023-03-30 09:52:37 +0100

    Using event in customize button component

--- components/src/App.js
+++ components/src/App.js
@@ -1,39 +1,61 @@
 import Button from "./Button";
-import { FaAirbnb } from "react-icons/fa";
+import {
+  FaAirbnb,
+  FaAffiliatetheme,
+  FaAlgolia,
+  FaBattleNet,
+  FaBuffer,
+  FaCottonBureau,
+  FaDelicious,
+} from "react-icons/fa";
 
 function App() {
+  const handleClick = () => {
+    console.log("Clicked!!!");
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello tailwind</h1>
       <div>
-        <Button success outline rounded>
+        <Button onClick={handleClick} success outline rounded>
           <FaAirbnb />
           Success outline rounded
         </Button>
       </div>
       <div>
-        <Button danger>Danger outline</Button>
+        <Button danger>
+          <FaAffiliatetheme />
+          Danger outline
+        </Button>
       </div>
       <div>
-        <Button warning>Warning</Button>
+        <Button warning>
+          <FaAlgolia />
+          Warning
+        </Button>
       </div>
       <div>
-        <Button secondary outline>
-          secondary outline
+        <Button danger rounded outline>
+          <FaBattleNet />
+          secondary rounded outline
         </Button>
       </div>
       <div>
         <Button secondary rounded>
+          <FaBuffer />
           secondary rounded
         </Button>
       </div>
       <div>
         <Button primary outline>
+          <FaCottonBureau />
           primary button
         </Button>
       </div>
       <div>
         <Button danger outline rounded>
+          <FaDelicious />
           primary button
         </Button>
       </div>
